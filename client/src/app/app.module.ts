a48715e5975855a0d56dc78551966e2002bf77e8
Khaled Awashreh <khaled.m.awashreh@gmail.com>
2021-02-22 16:46:18 +0200

    End of section 6, routing in angular, modules, themes, and more

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 
 @NgModule({
@@ -17,13 +23,17 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     HttpClientModule, BrowserAnimationsModule,
-    FormsModule, BsDropdownModule.forRoot()
+    FormsModule, SharedModule
 
 
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_Services/account.service';
 
 @Component({
@@ -13,7 +14,7 @@ export class RegisterComponent implements OnInit {
 
 
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService ,private toastr: ToastrService ) { }
 
   ngOnInit(): void {
   }
@@ -23,7 +24,13 @@ export class RegisterComponent implements OnInit {
 
       console.log(response);
       this.cancel();
-    })
+    }, error => {
+
+        this.toastr.error(error.error);}
+
+
+
+    ) 
   }
 
   cancel() {
